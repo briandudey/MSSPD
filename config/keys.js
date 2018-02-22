81cc9acd3ba5d7e7b64686bd75572f46dbb41337
Brian Dudey <dudey.brian@gmail.com>
2018-02-21 20:43:25 -0600

    round two w/ Google Auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://BrianDudey:pass123@ds143778.mlab.com:43778/storybooks310-dev',
+  googleClientID: '92501021842-h4r593g74d169890pl0ihd1ge4p5m8lo.apps.googleusercontent.com',
+  googleClientSecret: 'ARyapD7So8Ocb_v1MdeR-bHF'
+}
